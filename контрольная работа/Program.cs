c56349b03d46295e565be1cb4edd2c06ac5f093b
Igor Kuznetsov <kuznetcov28@mail.ru>
2023-03-14 14:17:43 +0300

    feat checking array

--- контрольная работа/Program.cs
+++ контрольная работа/Program.cs
@@ -1,4 +1,4 @@
-// Задача. Написать программу, которая из имеющегося массива строк
+﻿// Задача. Написать программу, которая из имеющегося массива строк
 // формирует массив из строк, длина которых меньше либо равна 3 символам.
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте 
 // выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
@@ -16,3 +16,16 @@ Clear();
 string[] array = AskArray();
 string[] result = FindLessThan(array, 3);
 WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+
+string[] FindLessThan(string[] input, int n) {
+    string[] output = new string[CountLessThan(input, n)];
+
+    for(int i = 0, j = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
